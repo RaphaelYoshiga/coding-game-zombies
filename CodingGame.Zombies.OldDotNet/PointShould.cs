e6a80d2233a9d7c96b260a8ce3b71e677bf70827
Raphael Yoshiga <ryoshiga@veinteractive.com>
2017-11-05 23:07:21 +0000

    Migrate to old .net framework for testing purposes

---
+++ CodingGame.Zombies.OldDotNet/PointShould.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace CodingGame.Zombies.OldDotNet
+{
+    [TestFixture]
+    public class PointShould
+    {
+
+        [Test]
+        public void CalculateDistance()
+        {
+            var point = new Point(0,0);
+            var anotherPoint = new Point(0, 1);
+
+            point.CalculateDistance(anotherPoint).ShouldBe(1);
+        }
+    }
+}
